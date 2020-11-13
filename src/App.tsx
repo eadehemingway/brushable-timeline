c124ab85b13e628c3086a3d66d93e01c327088f7
eadehemingway <eade.hemingway@gmail.com>
2020-11-13 14:05:05 +0000

    draw x axis

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { data } from './data'
+import * as d3 from 'd3'
+import './styles.css'
 
 function App() {
-  return <Container></Container>
+  const sidePadding = 100
+  const svgWidth = 800
+  const svgHeight = 500
+  function drawTimeline() {
+    const svg = d3
+      .select('svg')
+      .attr('height', svgHeight)
+      .attr('width', svgWidth)
+    // get min/max years of data
+    const startYears = data.map((d) => d.startYear)
+    const min = Math.min(...startYears)
+    const max = Math.max(...startYears)
+
+    // create x scale (linear)
+    const xScale = d3
+      .scaleTime()
+      .domain([min, max])
+      .range([sidePadding, svgWidth - sidePadding])
+
+    // create y scale (three levels? discreet) (need scale but dont need axis)
+    var yScale = d3
+      .scaleLinear()
+      .domain([0, 3])
+      .range([svgHeight / 2, 0])
+
+    // draw the x axis
+    svg
+      .append('g')
+      .attr('class', 'axis')
+      .attr('transform', 'translate(0,' + svgHeight / 2 + ')')
+      .call(d3.axisBottom(xScale))
+
+    // then plot points (as if scatter graph)
+  }
+
+  useEffect(() => {
+    drawTimeline()
+  }, [])
+
+  return (
+    <Container>
+      <Svg />
+    </Container>
+  )
 }
 
 export default App
@@ -11,4 +56,10 @@ export default App
 const Container = styled.div`
   background-color: #282c34;
   height: 100vh;
+  display: grid;
+  justify-items: center;
+`
+const Svg = styled.svg`
+  border: 2px solid linen;
+  margin-top: 100px;
 `
--- src/data.ts
+++ src/data.ts
@@ -1,14 +1,14 @@
 export const data = [
   {
-    startDate: 1865,
-    endDate: null,
+    startYear: 1865,
+    endYear: null,
     title: '13th Ammendment to the constitution',
     description: `Grants freedom to all americans accept those being punished for a crime. Approximately 4 million saves freed in America`,
     level: 1,
   },
   {
-    startDate: 1865,
-    endDate: 'ongoing',
+    startYear: 1865,
+    endYear: 'ongoing',
     title: 'African Americans arrested on mass',
     description: `The loophole in the ammendment allowing the incarceration as a form of punishment was immediately exploited. 
         African Americans arrested on mass for minor crimes such as loitering or being unemployed. 
@@ -17,15 +17,15 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1877,
-    endDate: null,
+    startYear: 1877,
+    endYear: null,
     title: 'Jim Crow laws began',
     description: `Laws that enforced racial segregation as well as preventing black people from voting.`,
     level: 1,
   },
   {
-    startDate: 1915,
-    endDate: null,
+    startYear: 1915,
+    endYear: null,
     title: 'The Birth of a Nation released in cinemas.',
     description: `One of the first big blockbuster films. Painted the picture of the civil war that the whites wanted to see.
       Every black person in it portrayed as animal like and canibalistic. Almost directly responsible for the rebirth of the KKK
@@ -34,8 +34,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1916,
-    endDate: null,
+    startYear: 1916,
+    endYear: null,
     title: 'Rise in Lynchings and the start of The Great Migration',
     description: `The Birth of a Nation led to a wave of terrorism. Lynchings rose and thousands of African 
       Americans were killed. The demographic of the USA is defined by this. Black people were refugees 
@@ -44,32 +44,32 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1954,
-    endDate: null,
+    startYear: 1954,
+    endYear: null,
     title: 'Civil rights movement starts',
     description: `
       `,
     level: 1,
   },
   {
-    startDate: 1955,
-    endDate: null,
+    startYear: 1955,
+    endYear: null,
     title: 'Rosa Parks refuses to give up her seat',
     description: `
       `,
     level: 2,
   },
   {
-    startDate: 1964,
-    endDate: null,
+    startYear: 1964,
+    endYear: null,
     title: 'Civil Rights Act',
     description: `
       `,
     level: 1,
   },
   {
-    startDate: 1968,
-    endDate: null,
+    startYear: 1968,
+    endYear: null,
     title: 'Fred Hampton killed in his bed by police',
     description: `
   Fred Hampton managed to pull together Blacks, Whites, Puerto Ricans and Native americans for justice at the age of 21. 
@@ -78,8 +78,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1968,
-    endDate: null,
+    startYear: 1968,
+    endYear: null,
     title: "Nixon's Southern Strategy aims to recruit racist voters",
     description: `
   Nixon begins to recruit Southern whites by speaking to thinly veiled racist issues. He talks about crime, 
@@ -88,8 +88,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1970,
-    endDate: null,
+    startYear: 1970,
+    endYear: null,
     title: '"War on Crime" and the Era of Mass Encarceration',
     description: `
       Nixon introduces the idea of a 'War on Crime'. Example of 'Dog Whistle Politics' - coded language which appears 
@@ -100,8 +100,8 @@ export const data = [
     level: 1,
   },
   {
-    startDate: 1971,
-    endDate: null,
+    startYear: 1971,
+    endYear: null,
     title:
       '"War on Drugs" and the Era of dealing with drug addiction as a crime',
     description: `
@@ -115,8 +115,8 @@ export const data = [
     level: 1,
   },
   {
-    startDate: 1981,
-    endDate: null,
+    startYear: 1981,
+    endYear: null,
     title: 'Reagans campaign strategist describes Southern Strategy',
     description: `
   Lee Atwater, Reagans campaign strategist caught on tape explaining the Southern strategy: "you start out in 1954 by 
@@ -127,16 +127,16 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1981,
-    endDate: null,
+    startYear: 1981,
+    endYear: null,
     title: 'Reagan becomes president',
     description: `
       `,
     level: 1,
   },
   {
-    startDate: 1983,
-    endDate: null,
+    startYear: 1983,
+    endYear: null,
     title: 'Corrections Corporations of America created',
     description: `
       CCA was the first private prison corporation in the US. Their incentive is to increase prison population 
@@ -147,8 +147,8 @@ export const data = [
     level: 1,
   },
   {
-    startDate: 1985,
-    endDate: null,
+    startYear: 1985,
+    endYear: null,
     title: 'Crack cocain arrives',
     description: `
   Crack cocain is much cheaper and therefore more profitable than cocain. It is 
@@ -157,8 +157,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1986,
-    endDate: null,
+    startYear: 1986,
+    endYear: null,
     title: 'Anti Drug Abuse Act',
     description: `
   Reagan establises mandatory where the same amount of time in prison for once ounce of crack cocaine was given for 
@@ -167,8 +167,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1986, // generally in the 80's
-    endDate: 1989,
+    startYear: 1986, // generally in the 80's
+    endYear: 1989,
     title: 'Black people being portrayed as criminals',
     description: `
   News and tv shows show black people as criminals at a disproportionate rate. The term 'Super predator' starts being used. 
@@ -177,8 +177,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1989,
-    endDate: null,
+    startYear: 1989,
+    endYear: null,
     title: 'Central Park Five',
     description: `
   Five black kids sent to prison for the rape of a white woman. All of whom were later found innocent after each spending 
@@ -188,8 +188,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1989,
-    endDate: null,
+    startYear: 1989,
+    endYear: null,
     title:
       "Bush's election campaign focuses on creating fear of black criminals",
     description: `
@@ -200,8 +200,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1989, // not sure hwen but in general period before next election in 1993?
-    endDate: 1993,
+    startYear: 1989, // not sure hwen but in general period before next election in 1993?
+    endYear: 1993,
     title: 'Politicians compete on how tough they are going to be on crime',
     description: `
   Once the fear of crime has been created it becomes almost impossible for a politician to appear 'soft on crime', so 
@@ -210,16 +210,16 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 1993,
-    endDate: null,
+    startYear: 1993,
+    endYear: null,
     title: 'Clinton comes to power',
     description: `
       `,
     level: 1,
   },
   {
-    startDate: 1994,
-    endDate: null,
+    startYear: 1994,
+    endYear: null,
     title: '1994 Crime Bill',
     description: `
   Clinton assigns loads of money to expand the prison system. This crime bill included:
@@ -232,8 +232,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 2010,
-    endDate: null,
+    startYear: 2010,
+    endYear: null,
     title: 'CCA leave ALEC',
     description: `
       ALEC is a private club with politicians and corporations as members. 
@@ -245,8 +245,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 2012,
-    endDate: null,
+    startYear: 2012,
+    endYear: null,
     title: 'Trayvon Martin shot for looking suspicious',
     description: `
   Trayvon Martin was shot by a civilian for looking suspicious. Martin died from his injuries but the civilian claimed
@@ -255,8 +255,8 @@ export const data = [
     level: 2,
   },
   {
-    startDate: 2015,
-    endDate: null,
+    startYear: 2015,
+    endYear: null,
     title:
       'March in Selma Alabama to mark the 50th anniversary of the Civil Rights March in 1965',
     description: `

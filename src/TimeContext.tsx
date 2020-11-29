1029a8e7b40175b6026956356b68a56ba17bc62b
eadehemingway <eade.hemingway@gmail.com>
2020-11-29 15:42:26 +0000

    creatte empty asvgs for the other graphs

---
+++ src/TimeContext.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import _ from 'lodash'
+
+export function TimeContext() {
+  return <Svg />
+}
+
+const Svg = styled.svg`
+  width: 80vw;
+  height: 400px;
+  border: 1px solid #f9a03f;
+  margin: 30px;
+`
